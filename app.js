32fef1e9c61a640213421812a53844604ac48f0f
talhaekrem <talha.ekrem.99@gmail.com>
2026-01-04 18:45:12 +0300

    ders26

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const courseRoute = require("./routes/course.route");
 const categoryRoute = require("./routes/category.route");
 const userRoute = require("./routes/user.route");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 //db connect
